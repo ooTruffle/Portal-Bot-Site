5b0e59c3e9daa2267f64618ab7b3d73439f9cfa9
ooTruffle <Truffle_YT@hotmail.com>
2023-12-20 22:46:30 +1300

    fix bwa

--- link/script.js
+++ link/script.js
@@ -64,7 +64,7 @@ function sendMessage() {
         console.log('Embed:', embed);
 
         // Send the message to the Discord bot using a fetch request
-        fetch('http://localhost:3539/sendMessage', {
+        fetch('http://161.97.159.175:3539/sendMessage', {
         method: 'POST',
         headers: {
         'Content-Type': 'application/json',
